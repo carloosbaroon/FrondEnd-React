850dac8d661c9d84b902fc9c987f386d1c7898df
Carlos Baron <xbtbaron@gmail.com>
2023-08-25 16:14:23 -0600

    Adding reactstrap with yarn

--- confusion/src/index.js
+++ confusion/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//Adding bootstrap to our project, it's before index.css in case we want
+// to overide the behaivor of bootstrap components in our index.css
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Here were are rendering App component and
+// attach it to root element that is defined in our index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
