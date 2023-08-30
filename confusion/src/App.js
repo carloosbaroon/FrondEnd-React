98fd9cebb1752aca4fca602879fb2937ef4340b7
Carlos Baron <carlostectol@gmail.com>
2023-08-30 13:17:08 -0600

    Testing email on github

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -3,7 +3,7 @@ import Main from "./components/MainComponent";
 import './App.css';
 
 class App extends Component {
-
+//Testing email for github
     render() {
         return (
             //These classNames are defined in the App.css
